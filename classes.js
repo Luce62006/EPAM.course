10b1579164f5f92cdad0914d054e3e81beb39458
SvetlanaFoglia <luce62006@gmail.com>
2023-03-02 23:56:21 +0100

    classes

--- classes.js
+++ classes.js
@@ -1,11 +1,21 @@
 class Animal {
     move = true
+    constructor(sound) {
+        this.sound = sound;
+    }
+    say() {
+        console.log(`${this.sound}`)
+    }
 }
 
 class Cat extends Animal{
+    constructor(sound) {
+        super(sound);
+    }
     color = 'black'
 }
 
-let Vasily =  new Cat();
-console.log(Vasily) //Cat { move: true, color: 'black' }
+let Vasily =  new Cat("miay");
+console.log(Vasily) //Cat  { move: true, sound: 'miay', color: 'black' }
+
 
